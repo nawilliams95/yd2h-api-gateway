ab238c506fcb34e0dc21b02dfff4f41c18f802c7
Nykarri <karri.bonez@gmail.com>
2020-09-30 16:34:21 -0400

    confi user accout service + .env

--- services/userAccouts.js
+++ services/userAccouts.js
@@ -1,33 +1,51 @@
 var express = require('express');
-var userRouter = express.Router()
+var userRouter = express.Router();
+const apiAdapter = require('../routers/apiAdapter');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const BASE_URL = process.env.USER_ACCOUNTS_URL
+const api = apiAdapter(BASE_URL)
 
 //INDEX
 userRouter.get('/users', (req, res) => {
-  res.send(req.path + " called")
+    api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
 })
 
 //SHOW
 userRouter.get('/users/:id', (req, res) => {
-  res.send(req.path + " called")
+    api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
 })
 
 //CREATE
 userRouter.post('/users', (req, res) => {
-  res.send(req.path + " called")
+   api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
 })
 
 //LOG-IN
 userRouter.post('/users/login', (req, res) => {
-    res.send(req.path + " called")
+     api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
 })
 
 //UPDATE
 userRouter.put('/users/:id', (req, res) => {
-    res.send(req.path + " called")
+     api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
 })
 
 //DELETE
 userRouter.delete('/users/:id', (req, res) => {
-    res.send(req.path + " called")
+     api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
   })
 module.exports = userRouter
