f68f77bacbe7afa997903b075e816ab6a6c5a676
Nykarri <karri.bonez@gmail.com>
2020-09-30 17:07:40 -0400

    confic tagging service routes

--- routers/router.js
+++ routers/router.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router()
 const userAccountService = require('../services/userAccouts');
-const userCMSService = require('../services/userCms')
+const userCMSService = require('../services/userCms');
+const taggingService = require('../services/tagging');
 
 router.use((req, res, next) => {
     console.log("Called: ", req.path)
     next()
 })
 
-router.use(userAccountService)
-router.use(userCMSService)
+router.use(userAccountService);
+router.use(userCMSService);
+router.use(taggingService);
+
+
 module.exports = router
---
+++ services/tagging.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const taggingRouter = express.Router();
+const apiAdapter = require('../routers/apiAdapter');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const BASE_URL = process.env.TAGGING_URL
+const api = apiAdapter(BASE_URL)
+
+//INDEX
+taggingRouter.get('/tags', (req, res) => {
+    api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
+})
+
+//SHOW
+taggingRouter.get('/tags/:id', (req, res) => {
+    api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
+})
+
+//CREATE
+taggingRouter.post('/tags', (req, res) => {
+   api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
+})
+
+//UPDATE
+taggingRouter.put('/tags/:id', (req, res) => {
+     api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
+})
+
+//DELETE
+taggingRouter.delete('/tags/:id', (req, res) => {
+     api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
+  })
+module.exports = taggingRouter
