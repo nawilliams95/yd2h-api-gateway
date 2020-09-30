0d1833ddf9d57d9eeaffcb7bccda4cf17d716b7d
Nykarri <karri.bonez@gmail.com>
2020-09-30 16:08:40 -0400

    made router

---
+++ routers/router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router()
+
+
+router.use((req, res, next) => {
+    console.log("Called: ", req.path)
+    next()
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const router = require('./routers/router');
 
 app.use(express.json());
 
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
     res.send("Simple API Gateway")
 })
 
+app.use(router)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(' Big Brother is listening.....');
