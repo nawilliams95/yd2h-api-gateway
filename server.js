cddb6d187a1728cff0be29678a5fa14bea9c9732
Nykarri <karri.bonez@gmail.com>
2020-10-03 17:13:10 -0400

    added cors lol forgot

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const router = require('./routers/router');
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send("Simple API Gateway For YD2H")
